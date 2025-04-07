fa886da60ffb0e5a301d0c1af2f1c0172b1ab4a0
Thomas Cali <tcali@bess-f4r1s12.clusters.42paris.fr>
2025-04-07 16:27:10 +0200

    added a comment which describes how redirection signs and pipe works. >> in pipex.c

--- src/pipex.c
+++ src/pipex.c
@@ -6,11 +6,15 @@
 /*   By: tcali <tcali@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/07 13:50:41 by tcali             #+#    #+#             */
-/*   Updated: 2025/04/07 14:29:50 by tcali            ###   ########.fr       */
+/*   Updated: 2025/04/07 16:25:54 by tcali            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
+
+// '<' takes the file as an input from the stdin
+// '|' redirects output of the previous cmd to the input of the next cmd
+// '>' "outfile redirection" writes the output of the last cmd into the outfile 
 
 int	main(int ac, char **av)
 {
